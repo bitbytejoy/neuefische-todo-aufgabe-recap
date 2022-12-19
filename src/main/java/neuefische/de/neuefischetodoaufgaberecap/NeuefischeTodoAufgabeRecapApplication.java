0f6c613ad5030b9256a9fd283414be8cf5fce524
bitbytejoy <bitbytejoy@gmail.com>
2022-12-19 10:20:16 +0100

    init: BIG BANG

---
+++ src/main/java/neuefische/de/neuefischetodoaufgaberecap/NeuefischeTodoAufgabeRecapApplication.java
@@ -0,0 +1,13 @@
+package neuefische.de.neuefischetodoaufgaberecap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeuefischeTodoAufgabeRecapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeuefischeTodoAufgabeRecapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/neuefische/de/neuefischetodoaufgaberecap/NeuefischeTodoAufgabeRecapApplicationTests.java
@@ -0,0 +1,13 @@
+package neuefische.de.neuefischetodoaufgaberecap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeuefischeTodoAufgabeRecapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
